813276a338c52b89bed1f976c28ea79f8d917792
1fexd <PGFvy85nC@protonmail.com>
2023-03-03 18:38:18 +0100

    fix: Use classLoader

--- src/main/kotlin/fe/fastforwardkt/Resource.kt
+++ src/main/kotlin/fe/fastforwardkt/Resource.kt
@@ -1,5 +1,5 @@
 package fe.fastforwardkt
 
 import java.io.InputStream
-
-fun getBuiltInJson(name: String = "/rules.json"): InputStream? = object {}::class.java.getResourceAsStream(name)
+object Resource
+fun getBuiltInJson(name: String = "rules.json"): InputStream? = Resource::class.java.classLoader.getResourceAsStream(name)
