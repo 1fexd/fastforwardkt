ca82a0541634d0640f232d171c68cfbbfdb57956
1fexd <PGFvy85nC@protonmail.com>
2023-06-12 18:52:07 +0200

    fix: Broken tests

--- src/test/kotlin/fe/fastforwardkt/GetRuleRedirectTest.kt
+++ src/test/kotlin/fe/fastforwardkt/GetRuleRedirectTest.kt
@@ -1,26 +1,23 @@
 package fe.fastforwardkt
 
-import FastForwardResource.getBuiltInFastForwardJson
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class GetRuleRedirectTest {
     @Test
     fun testGetRuleRedirectTest() {
-        val obj = FastForwardLoader.loadFastForwardJson(getBuiltInFastForwardJson()!!)
         assertEquals(
             "https://ninachuba.bio.to/glas-album",
             getRuleRedirect(
                 "https://www.youtube.com/redirect?event=video_description&redir_token=TOKEN_LEL&q=https%3A%2F%2Fninachuba.bio.to%2Fglas-album&v=D3RG2O03wsI",
-                obj,
                 debugPrint = true
             )
         )
+
         assertEquals(
             null,
             getRuleRedirect(
                 "https://www.humblebundle.com/games/humble-heroines-warriors-dreamers-and-god-slayers?partner=caschysblog",
-                obj,
                 debugPrint = true
             )
         )
--- src/test/kotlin/fe/fastforwardkt/IsTrackerTest.kt
+++ src/test/kotlin/fe/fastforwardkt/IsTrackerTest.kt
@@ -1,18 +1,16 @@
 package fe.fastforwardkt
 
-import FastForwardResource.getBuiltInFastForwardJson
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class IsTrackerTest {
     @Test
     fun testIsTracker() {
-        val obj = FastForwardLoader.loadFastForwardJson(getBuiltInFastForwardJson()!!)
-        assertEquals(true, isTracker("https://t.co/coInA0UU8T", obj))
-        assertEquals(true, isTracker("https://x.t.co/coInA0UU8T", obj))
-        assertEquals(true, isTracker("https://rebrand.ly/hallo", obj))
-        assertEquals(true, isTracker("https://shareasale-analytics.com/r.cfm?yeet=test", obj))
-        assertEquals(false, isTracker("https://lol.shareasale-analytics.com/r.cfm?yeet=test", obj))
-        assertEquals(true, isTracker("https://T.CO/coInA0UU8T", obj))
+        assertEquals(true, isTracker("https://t.co/coInA0UU8T"))
+        assertEquals(true, isTracker("https://x.t.co/coInA0UU8T"))
+        assertEquals(true, isTracker("https://rebrand.ly/hallo"))
+        assertEquals(true, isTracker("https://shareasale-analytics.com/r.cfm?yeet=test"))
+        assertEquals(false, isTracker("https://lol.shareasale-analytics.com/r.cfm?yeet=test"))
+        assertEquals(true, isTracker("https://T.CO/coInA0UU8T"))
     }
 }
