fb4d561aef1575f5bf75ad783d2d9521dec3d647
1fexd <PGFvy85nC@protonmail.com>
2023-06-22 22:23:26 +0200

    feat: Update FastForwardRules

--- src/main/kotlin/FastForwardResource.kt
+++
@@ -1,6 +0,0 @@
-import java.io.InputStream
-
-object FastForwardResource {
-    fun getBuiltInFastForwardJson(name: String = "fastforward_rules.json"): InputStream? =
-        FastForwardResource::class.java.getResourceAsStream(name)
-}
