96050acb2d73174a449cc53212f993c8427e8736
1fexd <PGFvy85nC@protonmail.com>
2023-12-11 07:50:48 +0100

    chore: Move updater to down diectory

--- update/fetch_latest.sh
+++ update/fetch_latest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DOWNLOAD_URL=$(curl -ILs -o /dev/null -w %{url_effective} https://github.com/1fexd/fastforward-ext/releases/latest | sed "s/tag/download/")
-wget "${DOWNLOAD_URL}/FastForwardRules.kt" -O ../kotlin/fe/fastforwardkt/FastForwardRules.kt
+wget "${DOWNLOAD_URL}/FastForwardRules.kt" -O ../src/main/kotlin/fe/fastforwardkt/FastForwardRules.kt
