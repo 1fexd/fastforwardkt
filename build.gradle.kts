da712daf5fa6be090589e4fa6feda1d31af6d5b8
1fexd <PGFvy85nC@protonmail.com>
2024-01-07 17:06:54 +0100

    chore: Move to shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
-    kotlin("jvm") version "1.8.21"
-    java
+    kotlin("jvm") version "1.9.22"
+    `java-library`
     `maven-publish`
     id("net.nemerosa.versioning") version "3.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "fe.fastforwardkt"
@@ -13,14 +16,37 @@ repositories {
     maven(url="https://jitpack.io")
 }
 
+val implementation: Configuration by configurations
+val shadowImplementation: Configuration by configurations.creating
+implementation.extendsFrom(shadowImplementation)
+
 dependencies {
-    implementation("com.github.1fexd:uriparser:0.0.7")
+    api(kotlin("stdlib"))
+
+    shadowImplementation("com.github.1fexd:uriparser:0.0.10")
 
     testImplementation(kotlin("test"))
 }
 
-tasks.withType<Jar> {
-    exclude("fetch_latest.sh")
+kotlin {
+    jvmToolchain(17)
+}
+
+val shadowJarTask = tasks.named<ShadowJar>("shadowJar") {
+    mergeServiceFiles()
+    exclude("META-INF/**/*", "*.sh")
+
+    listOf("fe").forEach { pkg ->
+        relocate(pkg, "${project.group}.internal$pkg")
+    }
+
+    archiveClassifier.set("")
+    minimize()
+    configurations = listOf(shadowImplementation)
+}
+
+tasks.named("jar").configure {
+    enabled = false
 }
 
 tasks.test {
@@ -29,12 +55,17 @@ tasks.test {
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("shadow") {
+            setArtifacts(listOf(shadowJarTask.get()))
             groupId = project.group.toString()
             version = project.version.toString()
-
-            from(components["java"])
         }
+    }
+}
+
+tasks.whenTaskAdded {
+    if (name == "generateMetadataFileForPluginShadowPublication") {
+        dependsOn(shadowJarTask)
     }
 }
 
